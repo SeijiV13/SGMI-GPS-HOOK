d379d96457f37f1d21308992bfbce5278659ae98
Seiji Ralph Villafranca <seijiv13@outlook.com>
2020-06-04 20:37:07 +0800

    added webhook post request

--- webhook.php
+++ webhook.php
@@ -1,3 +1,18 @@
 <?php
-    file_put_contents('result.txt', implode(" ",$_GET), FILE_APPEND);
+
+    $url = 'https://smgi-back-dev.herokuapp.com/gps';
+    $data = $_GET;
+    
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    if ($result === FALSE) { /* Handle error */ }
+    var_dump($result);
 ?>
